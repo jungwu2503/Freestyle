54ca0d651105f739ba77d4a207fa37c1cab7fcd1
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2022-11-09 13:53:56 +0900

    Add files via upload

---
+++ src/DeveloperFrame.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+import java.net.*;
+
+import javax.swing.*;
+
+public class DeveloperFrame extends JFrame {
+	
+	//JPanel dPanel;
+	JLabel label;
+	JLabel label2;
+	JButton urlBtn;
+	JButton backBtn;
+	ImageIcon img = new ImageIcon(DeveloperFrame.class.getResource("/img/toMainBtn.png"));
+	ImageIcon img2 = new ImageIcon(DeveloperFrame.class.getResource("/img/toMainBtn2.png"));
+	ImageIcon frameIcon;
+	
+	DeveloperFrame() {
+		/*dPanel = new JPanel();
+		dPanel.setPreferredSize(null);		
+		dPanel.add(textArea);		
+		add(dPanel);*/
+		
+		frameIcon = new ImageIcon(MainFrame.class.getResource("img/SulIcon.png"));
+				
+		backBtn = new JButton(img);
+		backBtn.setRolloverIcon(img2);
+		backBtn.setBorderPainted(false);
+		//backBtn.setPreferredSize(getSize());
+		backBtn.setBounds(10, 10, 50, 50);
+		backBtn.addActionListener((e) -> {
+			if (e.getSource() == backBtn) {
+				dispose();
+				MainFrame mainFrame = new MainFrame();
+			}
+		});
+		
+		label = new JLabel("Developer : SuldenLion");
+		label.setBounds(100, 50, 300, 50);
+		label.setFont(new Font(null, Font.BOLD, 25));
+		
+		label2 = new JLabel("<html>위 프로그램의 모든 작업은 SuldenLion이 수행하였음.<br>디자인적인 요소나 심지어는 bgm으로 사용된 비트도 SuldenLion이 제작.<br>코딩 실력이 부족하므로 버그가 발견될 수 있음.<br>멀티플레이 모드나 데이터베이스 사용해서 기록저장 같은걸 구현해보는 것이 목표.<br><br>↓ SuldenLion 개발자 홈페이지 방문하기 ↓</html>");
+		label2.setBounds(90, 70, 300, 300);
+		label2.setFont(new Font(null, Font.PLAIN, 15));
+		
+		urlBtn = new JButton("<html><a href=\"https://jwsite.cf/\" title=\"SuldenLion HomePage 바로가기\">SuldenLion HomePage</a></html>");
+		urlBtn.setBounds(90, 335, 300, 40);
+		urlBtn.setFocusable(false);
+		urlBtn.setToolTipText("SuldenLion 홈페이지 바로가기");
+		urlBtn.addActionListener((e) -> {
+			if (e.getSource() == urlBtn) {	
+				try {
+					URI uri = new URI("https://jwsite.cf/");
+					Desktop dt = Desktop.getDesktop();
+					dt.browse(uri.resolve(uri));
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}			
+		}); 
+		
+		add(backBtn);
+		add(label);
+		add(label2);
+		add(urlBtn);
+		setTitle("개발자 정보");
+		setIconImage(frameIcon.getImage());
+		getContentPane().setBackground(Color.white);
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		setLayout(null);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+	}
+	
+}
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,16 +1,26 @@
 import java.awt.*;
+import java.io.*;
+
+import javax.sound.sampled.*;
 import javax.swing.*;
-import javax.swing.border.LineBorder;
+import javax.swing.border.*;
 
 public class MainFrame extends JFrame {
 
 	JButton newWindowBtn;
+	JButton descriptionBtn;
+	JButton developerInfo;
 	JLabel imgLabel;
 	ImageIcon imageIcon;
 	ImageIcon frameIcon;
 	LineBorder lb;
+	//EmptyBorder eb;
+	File mainBgmFile;
+	Clip mainBgmClip;
+	//FileInputStream fis; 
 	
 	MainFrame() {
+		
 		//imgLabel = new JLabel(new Icon("../img/suldenlion.jpeg", 30));
 		//image = Toolkit.getDefaultToolkit().getImage("suldenlion.jpeg");
 		imageIcon = new ImageIcon(MainFrame.class.getResource("img/GUIsuldenlion.png"));
@@ -23,26 +33,83 @@ public class MainFrame extends JFrame {
 		
 		newWindowBtn = new JButton("게임 시작");
 		//newWindowBtn.setSize(200, 150);
-		newWindowBtn.setBounds(140, 290, 200, 80);
+		newWindowBtn.setBounds(140, 280, 200, 80);
 		newWindowBtn.setFont(new Font(null, Font.BOLD, 30));
 		newWindowBtn.setFocusable(false);
-		
 		lb = new LineBorder(Color.blue, 3);
 		newWindowBtn.setBorder(lb);
+		newWindowBtn.addActionListener((e) -> {
+			if (e.getSource() == newWindowBtn) {
+				dispose();
+				audioOff();
+				// <게임 프레임 만들기>
+			}
+		});
+		
+		descriptionBtn = new JButton("게임 설명");
+		descriptionBtn.setBounds(140, 375, 200, 25);
+		descriptionBtn.setFont(new Font(null, Font.ITALIC, 20));
+		descriptionBtn.setFocusable(false);
+		descriptionBtn.setBorderPainted(false);
+		descriptionBtn.setContentAreaFilled(false);
+		/*eb = new EmptyBorder(5,3,5,3);
+		descriptionBtn.setBorder(eb);*/	
+		descriptionBtn.addActionListener((e) -> {
+			if (e.getSource() == descriptionBtn) {
+				dispose();
+				audioOff();
+				DescriptionFrame df = new DescriptionFrame();
+			}
+		});
+		
+		developerInfo = new JButton("개발자 정보");
+		developerInfo.setBounds(140, 405, 200, 25);
+		developerInfo.setFont(new Font(null, Font.ITALIC, 17));
+		developerInfo.setFocusable(false);
+		developerInfo.setBorderPainted(false);
+		developerInfo.setContentAreaFilled(false);
+		developerInfo.addActionListener((e) -> {
+			if (e.getSource() == developerInfo) {
+				dispose();
+				audioOff();
+				DeveloperFrame df = new DeveloperFrame();
+			}
+		});
 		
 		//add(imgLabel);
 		getContentPane().add(imgLabel);
 		add(newWindowBtn);
-				
+		add(descriptionBtn);
+		add(developerInfo);
+		
 		setTitle("SuldenLion의 Freestyle GUI 게임");
 		setIconImage(frameIcon.getImage());
 		getContentPane().setBackground(Color.white);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500,500);
 		setLayout(null);
+		setLocationRelativeTo(null);
 		//setLayout(new BorderLayout());
 		//add(newWindowBtn, BorderLayout.CENTER);
 		setVisible(true);
+		
+		audioOn();
+	}
+	
+	public void audioOn() {
+		try {
+			mainBgmFile = new File("SuldenBeat.wav");
+			mainBgmClip = AudioSystem.getClip();
+			mainBgmClip.open(AudioSystem.getAudioInputStream(mainBgmFile));
+			mainBgmClip.loop(Clip.LOOP_CONTINUOUSLY);
+			mainBgmClip.start();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+	
+	public void audioOff() {
+		mainBgmClip.stop();
 	}
 	
 }
